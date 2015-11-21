3430f9f5421ef7252e4c06896f5a52a8ebc1bf57
Edward Dumholt <edumholt@gmail.com>
2015-11-21 14:53:14 -0600

    add warning text when ship is too low

--- js/ascend.js
+++ js/ascend.js
@@ -2,9 +2,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create:
 
 var cursors,
     scoreText,
+    warningText,
+    shipStats,
     starfield,
     ship;
 
+// Required to load Google web font
 WebFontConfig = {
 
     active: function() {
@@ -22,6 +25,7 @@ function preload() {
     game.load.image('starfield', 'assets/starfield.png');
     game.load.spritesheet('ship', 'assets/ship_sprites.png', 45, 52);
 
+    // Load Google web font 'Audiowide'
     game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
 
 }
@@ -32,18 +36,21 @@ function create() {
 
     ship = game.add.sprite(400, 400, 'ship');
     game.physics.arcade.enable(ship);
-    ship.body.gravity.y = 120;
+    ship.body.gravity.y = 140;
     ship.body.collideWorldBounds = true;
+    ship.anchor.x = ship.anchor.y = 0.5;
 
     ship.animations.add('left', [3, 2, 1, 0], 8, true);
     ship.animations.add('right', [0, 1, 2, 3], 8, true);
 
     cursors = game.input.keyboard.createCursorKeys();
+
+    warningText = game.add.text(16, game.world.height - 40, '', {font: '400 18px Audiowide', fill: '#F33', align: 'center'});
 }
 
 function update() {
 
-    starfield.tilePosition.y += 0.6;
+    starfield.tilePosition.y += 0.4;
 
     ship.body.velocity.x = 0;
 
@@ -58,10 +65,25 @@ function update() {
         ship.frame = 2;
     }
 
+    if(cursors.up.isDown) {
+        ship.body.velocity.y = -70;
+        ship.frame = 1;
+    }
+
+    if(ship.y >= game.world.height - 80) {
+        warningText.text = "WARNING: ACCELERATE SHIP";
+    } else {
+        warningText.text = '';
+    }
+
+
+    if(ship.y >= game.world.height - 30 ) {
+        ship.destroy();
+    }
+
 }
 
 function createText() {
-
     scoreText = game.add.text(16, 16, "SCORE: 0", {font: '400 24px Audiowide', fill: '#9F9'});
-
 }
+
