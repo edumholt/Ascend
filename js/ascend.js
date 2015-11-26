3f6ff09951d7896d7d269bc3731bf709639f0de2
Edward Dumholt <edumholt@gmail.com>
2015-11-26 00:01:31 -0600

    add bullets, bulletSound

--- js/ascend.js
+++ js/ascend.js
@@ -2,9 +2,13 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create:
 
 var asteroids,
     bgSound,
+    bulletSound,
+    bullets,
+    bulletTime = 0,
     cursors,
     dangerZone,
     explodeShip,
+    fireButton,
     scoreText,
     warningText,
     shipStats,
@@ -27,10 +31,12 @@ function preload() {
 
     game.load.image('starfield', 'assets/starfield.png');
     game.load.image('dangerZone', 'assets/DoNotEnter.png');
+    game.load.image('bullet', 'assets/bullet.png');
     game.load.spritesheet('asteroid', 'assets/asteroid.png', 36, 36);
     game.load.spritesheet('ship', 'assets/ship_sprites.png', 45, 52);
     game.load.spritesheet('shipExplosion', 'assets/explosion.png', 100, 100);
     game.load.audio('bg', 'assets/bg.mp3');
+    game.load.audio('bulletSound', 'assets/bulletSound.mp3');
 
     // Load Google web font 'Audiowide'
     game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
@@ -51,6 +57,16 @@ function create() {
 
     ship.animations.add('rotateTurrets', [], 30, true);
 
+    // Create bullets group
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 1);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+
     // Create asteroids group
     asteroids = game.add.group();
     asteroids.enableBody = true;
@@ -63,10 +79,13 @@ function create() {
 
     dangerZone = game.add.sprite(0, game.height - 50, 'dangerZone');
 
+    // Game controls
     cursors = game.input.keyboard.createCursorKeys();
+    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-    warningText = game.add.text(16, 40, '', {font: '400 20px Audiowide', fill: '#F33', align: 'center'});
+    warningText = game.add.text(16, 56, '', {font: '400 20px Audiowide', fill: '#F33', align: 'center'});
 
+    bulletSound = game.add.audio('bulletSound');
     bgSound = game.add.audio('bg');
     bgSound.play('', 0, 1, true);
 }
@@ -96,6 +115,11 @@ function update() {
             ship.body.velocity.y = -70;
             ship.animations.play('rotateTurrets');
         }
+
+        // Fire bullets
+        if(fireButton.isDown) {
+            fireBullet();
+        }
     }
 
     if(ship.y >= game.world.height - 160) {
@@ -121,6 +145,22 @@ function createRandomAsteroid() {
         asteroid.body.velocity.setTo(Math.random() * 60 - 30, Math.random() * 30 + 20);
         asteroid.animations.add('spin', [], 10, true);
         asteroid.play('spin');
+    }
+
+}
+
+function fireBullet() {
+    // To prevent bullets from being fired too fast, we set a time limit
+    if(game.time.now > bulletTime) {
+        // Get the first bullet we can from the pool
+        bullet = bullets.getFirstExists(false);
+        if(bullet) {
+            //And fire it
+            bullet.reset(ship.x, ship.y - 12);
+            bullet.body.velocity.y = -400;
+            bulletSound.play();
+            bulletTime = game.time.now + 200;
+        }
     }
 
 }
